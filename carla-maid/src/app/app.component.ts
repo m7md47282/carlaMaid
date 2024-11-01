d7a890a25b1673cb7bd5f947def8d3be56151763
mohammad omari <m.omari@classera.com>
2024-11-01 14:11:51 +0300

    hanfing directiona ,languages and environmant

--- carla-maid/src/app/app.component.ts
+++ carla-maid/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { Languages } from './shared/interfaces/languages';
 import { ConfigService } from './shared/config/config.service';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,37 +11,31 @@ import { ConfigService } from './shared/config/config.service';
   imports: [
     RouterOutlet,
     TranslateModule,
-    
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'carla-maid';
-  environment ={lang: 'en'};
-  textDir : string ='ltr'
 
-  constructor(private translate: TranslateService ,private configService: ConfigService){
-    // this.setLang('en')
-    this.translate.onLangChange.subscribe((event: LangChangeEvent) =>
-      {
-        if(event.lang == 'ar')
-        {
-          this.textDir = 'rtl';
-        } 
-        else
-        {
-          this.textDir = 'ltr';
-        }
-      });
-  }
-  setLang(lang: any){
+  lang: Languages = environment.lang as Languages;
+
+  constructor(
+    private _configService: ConfigService
+    ) {
    
-      localStorage.setItem('lang', lang);
-      document.documentElement.lang = lang;
-      this.environment.lang = lang;
-      this.translate.use(this.environment.lang);
-
   }
+  ngOnInit(): void {}
+
+  toggleLang(){
+    this._configService.getLang() === 'ar'? this.setLang('en') : this.setLang('ar');
+
+    this.lang = this._configService.getLang()
+  }
+  setLang(lang: any) {
+    this._configService.setLang(lang);
+  }
+
 
 }
