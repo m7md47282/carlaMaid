2906979e02dd2ba96e7934ec620e19c5b0c79f02
abdallah alomari <abdallaalomari3666@gmail.com>
2024-11-04 17:57:22 +0300

    building the landing page

--- carla-maid/src/app/header/header.component.ts
+++ carla-maid/src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { ConfigService } from '../shared/config/config.service';
-
+import { MatIconModule } from '@angular/material/icon'
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    TranslateModule
+    TranslateModule,
+    MatIconModule 
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
