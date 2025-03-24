b10189239b73f4b1c1c731705b1c30aa4a78e3a8
Ahmad Alhafi <ahmaed.hakam@gmail.com>
2025-03-25 02:39:34 +0300

    implement dynamic wordpress blogs page

--- carla-maid/src/app/blogs-open/blogs-open.component.ts
+++ carla-maid/src/app/blogs-open/blogs-open.component.ts
@@ -1,23 +1,54 @@
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { WordPressService } from '../shared/services/word-press.service';
+import { SharedService } from '../shared/services/shared.service';
 @Component({
   selector: 'app-blogs-open',
   standalone: true,
   imports: [
     TranslateModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './blogs-open.component.html',
   styleUrl: './blogs-open.component.sass'
 })
 export class BlogsOpenComponent {
-  lang: string ;
+  lang: string;
+  posts!: any[];
 
-  constructor(private translateService: TranslateService){
+  constructor(
+    private translateService: TranslateService,
+    private _wordPressService: WordPressService,
+    protected _sharedService: SharedService
+  ) {
+    
     this.lang = this.translateService.currentLang || 'en';
     this.translateService.onLangChange.subscribe(() => {
       this.lang = this.translateService.currentLang || 'en';
     });
+
+    this.getPosts();
   }
+
+  /**
+   * Retrieves posts from the WordPress service with the specified parameters.
+   */ 
+  getPosts(): void {
+    const params = {
+      per_page: 100,
+      page: 1,
+      orderby: 'date',
+      order: 'asc'
+    };
+
+    const preparedParams = this._sharedService.prepareParams({ params: params });
+
+    this._wordPressService.getPosts(preparedParams).subscribe({
+      next: (value: any) => {
+        this.posts = value;
+      }
+    });
+  }
+
 }
--- carla-maid/src/environments/environment.ts
+++ carla-maid/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false, // Indicates if the environment is production
     // apiUrl: '',
+    wordpressApiBaseUrl: 'https://carlamaid.qa/wordpress/index.php/wp-json/wp/v2', 
     lang: 'en', // Default language setting
     languages: ['ar', 'en'], // Supported languages
     direction: 'ltr', // Text direction, left-to-right
