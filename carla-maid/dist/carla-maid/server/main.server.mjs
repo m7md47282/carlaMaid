8ced57c84734e06f6024548ef6c25eca576bef9b
abdallah alomari <abdallaalomari3666@gmail.com>
2025-04-18 01:42:52 +0300

    fix

--- carla-maid/dist/carla-maid/server/main.server.mjs
+++ carla-maid/dist/carla-maid/server/main.server.mjs
@@ -1,12 +1,12 @@
 import './polyfills.server.mjs';
 import {
   main_server_default
-} from "./chunk-LQ7473BZ.mjs";
+} from "./chunk-52WUYHJJ.mjs";
 import "./chunk-CHQJYM7W.mjs";
 import "./chunk-RJMWU5WN.mjs";
 import "./chunk-AKN435VV.mjs";
 import "./chunk-NPJDOPSY.mjs";
-import "./chunk-PLKUX7CK.mjs";
+import "./chunk-GAEUWEXE.mjs";
 import "./chunk-UFOZR5SN.mjs";
 import "./chunk-4Q3HI7BM.mjs";
 import "./chunk-OKUDJCYY.mjs";
